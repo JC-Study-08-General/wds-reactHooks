61a509382bf03bcd5c068d02d9711e9a8960effa
Justin <just.coder@outlook.com>
2024-02-15 20:05:29 +0000

    feat: started app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useIntlCommon } from "./_utils/lang/intl-common";
 
-function App() {
+import AboutPageView from "./presentation/AboutPage/pages/AboutPageView";
+import AppendixPageView from "./presentation/AppendixPage/pages/AppendixPageView";
+import GalleryPageView from "./presentation/GalleryPage/pages/GalleryPageView";
+import HistoryPageView from "./presentation/HistoryPage/pages/HistoryPageView";
+import HomePageView from "./presentation/HomePage/pages/HomePageView";
+import LandingPageView from "./presentation/LandingPage/pages/LandingPageView";
+import UserPageView from "./presentation/User/pages/UserPageView";
+import PageNotFoundView from "./presentation/_AuthenticatedLayout/pages/PageNotFound/PageNotFoundView";
+
+const App = () => {
+  const { siteLabel } = useIntlCommon();
+
+  useEffect(() => {
+    document.title = siteLabel;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<PageNotFoundView />} />
+        <Route path="/" element={<LandingPageView />} />
+        <Route path="/home" element={<HomePageView />} />
+        <Route path="/about" element={<AboutPageView />} />
+        <Route path="/appendix" element={<AppendixPageView />} />
+        <Route path="/gallery/picture" element={<GalleryPageView />} />
+        <Route path="/gallery/video" element={<GalleryPageView />} />
+        <Route path="/history" element={<HistoryPageView />} />
+        <Route path="/user" element={<UserPageView />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
