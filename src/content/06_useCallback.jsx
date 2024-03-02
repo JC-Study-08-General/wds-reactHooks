89277cdbabef8d23f58562260efdeb9cd0ef48d6
Justin Heath <just.coder@outlook.com>
2024-03-02 15:44:36 +0000

    refactor: useCallback

--- src/content/06_useCallback.jsx
+++ src/content/06_useCallback.jsx
@@ -1,13 +1,15 @@
-import { Box, Button, List, ListItem, Stack, Typography } from "@mui/material";
-import React, { useState } from "react";
+import { Box, Button, List, ListItem, Stack, TextField, Typography } from "@mui/material";
+import React, { useState, useEffect, useCallback } from "react";
 import MainLayout from "../_mainLayout/pages/MainLayoutView";
 
 function ExampleCallback() {
-  const [something, setSomething] = useState(null);
+  const [something, setSomething] = useState(1);
+  const [dark, setDark] = useState(false);
 
-  function example() {
-    setSomething("example");
-  }
+  const example = useCallback(() => {
+    return [something, something + 1, something + 2];
+  }, [something]);
+
   return (
     <MainLayout>
       <Stack justifyContent={"center"} alignItems={"center"}>
@@ -16,24 +18,50 @@ function ExampleCallback() {
         </a>
         <Box sx={{ maxWidth: 800, margin: 2, backgroundColor: "bisque", padding: 7, borderRadius: 15 }}>
           <List sx={{ listStyleType: "disc" }}>
+            <ListItem sx={{ display: "list-item" }}>The same as useMemo, most occasion use useMemo.</ListItem>
+            <ListItem sx={{ display: "list-item" }}>Has to have a depency array.</ListItem>
             <ListItem sx={{ display: "list-item" }}>
-              <span style={{ fontWeight: "bold" }}>Example: </span> Example.
+              Caches the reference of the previous state and will only run if the reference has changed!!
+            </ListItem>
+            <ListItem sx={{ display: "list-item" }}>
+              The difference to useMemo is useCallback returns the entire function!!!
+            </ListItem>
+            <ListItem sx={{ display: "list-item" }}>
+              <span style={{ fontWeight: "bold" }}>Referential Equality: </span>Only runs if the reference has changed.
             </ListItem>
           </List>
         </Box>
         <Stack alignItems={"center"} sx={{ backgroundColor: "lightblue", padding: 8, borderRadius: 15 }}>
-          <Typography variant="h6" sx={{ marginBottom: 5 }}>
-            Example - {something}
-          </Typography>
-          <Stack direction={"row"} alignItems={"center"} gap={5}>
-            <Button variant="contained" onClick={() => example}>
+          <TextField
+            type="number"
+            label="something"
+            variant="outlined"
+            value={something}
+            onChange={(e) => setSomething(parseInt(e.target.value))}
+          />
+          <Stack direction={"row"} alignItems={"center"} gap={5} sx={{ marginTop: 5 }}>
+            <Button variant="contained" color={dark ? "primary" : "success"} onClick={() => setDark((d) => !d)}>
               Example
             </Button>
           </Stack>
+          <Box sx={{ marginTop: 5 }}>
+            <ListExample getItems={example} />
+          </Box>
         </Stack>
       </Stack>
     </MainLayout>
   );
+}
+
+function ListExample({ getItems }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    console.log("List has Ran.....");
+    setItems(getItems());
+  }, [getItems]);
+
+  return items.map((i) => <Box key={i}>{i}</Box>);
 }
 
 export default ExampleCallback;
